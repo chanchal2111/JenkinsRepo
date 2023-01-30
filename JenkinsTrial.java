0a54c96b43cf377bed1c5c3291a93b7133c7ea8e
chanchal2111 <122576775+chanchal2111@users.noreply.github.com>
2023-01-30 23:35:29 +0000

    This is a fresh commit

---
+++ JenkinsTrial.java
@@ -0,0 +1,10 @@
+public class JenkinsTrial {
+
+
+
+        public static void main(String[] args) {
+            System.out.println("I am learning Jenkinsss");
+            System.out.println("This is a demo for a successful Builld");
+        }
+    }
+
