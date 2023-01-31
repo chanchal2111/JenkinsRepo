7048467c6d370d08502fa7d83fe1a5909baf88f8
chanchal2111 <chanchal2111@gmail.com>
2023-01-31 21:28:36 +0000

    Commit the changes after clone

--- JenkinsTrial.java
+++ JenkinsTrial.java
@@ -1,10 +1,9 @@
 public class JenkinsTrial {
 
 
-
         public static void main(String[] args) {
             System.out.println("I am learning Jenkinsss");
-            System.out.println("This is a demo for a successful Builld");
+            System.out.println("This is a demo for a successful Build");
         }
     }
 
