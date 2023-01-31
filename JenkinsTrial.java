3c7e89e52a0463f0fe3c42a44d6e890bc6aff320
chanchal2111 <122576775+chanchal2111@users.noreply.github.com>
2023-01-31 22:25:40 +0000

    Update JenkinsTrial.java

--- JenkinsTrial.java
+++ JenkinsTrial.java
@@ -4,6 +4,7 @@ public class JenkinsTrial {
         public static void main(String[] args) {
             System.out.println("I am learning Jenkinsss today - 31 Jan 2023");
             System.out.println("This is a demo for a successful Build");
+                System.out.println("This will be a Green Build");
                 
         }
     }
